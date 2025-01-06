9e1f7129048e9952f457af0f0fec05bdc83fa5a9
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-01-06 22:35:56 +0530

    Create security-group file

---
+++ security-group.tf
@@ -0,0 +1,22 @@
+ name        = "new-securitygroup-for-terraform"
+  description = "allow ssh & http"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
