56b1ef85fcb0b9a19e3feb00e0ea93efd7e1af40
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-01-06 22:38:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "myweb12" {
   ami             = "ami-0fff1b9a61dec8a5f"
-  instance_type   = "t3.micro"
-  key_name        = "my-terra-key"
-  security_groups = ["allow_tls"]
+  instance_type   = "t2.micro"
+  key_name        = "terraform-key"
+  security_groups = ["new-sg-tf"]
   tags = {
     Name = "HR"
   }
