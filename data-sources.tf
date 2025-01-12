0aaea0dfb06e61cd429522c11a437380e598e86c
RAJAT KUMAR <kumar.rajat0978@gmail.com>
2025-01-12 19:00:35 +0530

    Create data-sources.tf
    Data Sources work to sense that we get information about  existing one but can't change them. It is pretty much as "read only" resources

---
+++ data-sources.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name  = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+
+}
+
+resource "aws_instance" "example" {
+
+  ami = data.aws_ami.amazon_linux.id
+
+  instance_type = "t3.micro"
+  tags = {
+    name = "example-instance"
+  }
+
+}
